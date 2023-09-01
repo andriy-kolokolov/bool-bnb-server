e0991e3a9a82aee1dcd6a0f05c830102d6109bb9
alessandropecchini99 <pecchini99@gmail.com>
2023-09-01 10:57:26 +0200

    add route

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -90,4 +90,11 @@ class ApartmentController extends Controller
 
         return response()->json($messages);
     }
+
+
+    public function getAppServ(): JsonResponse
+    {
+        $apartments = Apartment::with('services')->get();
+        return response()->json($apartments);
+    }
 }
