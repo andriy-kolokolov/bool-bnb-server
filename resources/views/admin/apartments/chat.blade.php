717b08998259c1dbe7769d533a4a610af3d9f4fc
andriy-kolokolov <koloandre22@gmail.com>
2023-09-05 09:42:03 +0200

    add row guest name

--- resources/views/admin/apartments/chat.blade.php
+++ resources/views/admin/apartments/chat.blade.php
@@ -1,15 +1,16 @@
 @extends('admin.layouts.base')
 
 @section('content')
-  <h1>Apartment Messages</h1>
-  <div class="d-flex flex-wrap">
-    @foreach($messages as $message)
-      <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h4 class="card-title">{{ $message->guest_email }}</h4>
-          <p class="card-text">{{ $message->message }}</p>
-        </div>
-      </div>
-    @endforeach
-  </div>
+    <h1>Apartment Messages</h1>
+    <div class="d-flex flex-wrap">
+        @foreach($messages as $message)
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h4 class="card-title">{{ $message->guest_name }}</h4>
+                    <h5 class="card-text">{{ $message->guest_email }}</h5>
+                    <p class="card-body">{{ $message->message }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
