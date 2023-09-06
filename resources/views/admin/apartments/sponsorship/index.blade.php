6108feeab5e359657e35d58b5a32be4a8fde4659
StefanoGalandrini <stefe68@gmail.com>
2023-09-06 13:27:17 +0200

    Add sponsoship cards style

--- resources/views/admin/apartments/sponsorship/index.blade.php
+++ resources/views/admin/apartments/sponsorship/index.blade.php
@@ -1,25 +1,38 @@
 @extends('admin.layouts.base')
 @section('content')
-    <div class="container mt-3">
-        <h2>Sponsorship for this Apartment</h2>
-        <div class="d-flex gap-4 justify-content-center mt-5 mb-5">
-            @foreach($availableSponsorships as $sponsorship)
-                <div class="card text-center w-25">
-                    <div class="card-header">
-                        <h3 class="m-0">{{ $sponsorship->level }}</h3>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $sponsorship->duration }}</h5>
-                        <h6 class="card-title mt-3 mb-3" style="color: #9153a9;">{{ $sponsorship->price }}</h6>
-                        <a href="#" class="btn myBtnPurple" style="background-color: #485ba1; color: white;">Buy</a>
-                    </div>
-                </div>
-            @endforeach
-        </div>
+	<div class="container mt-3">
+		<h2>Sponsorship for this Apartment</h2>
+		<div class="d-flex gap-4 justify-content-center mt-5 mb-5">
+			@foreach ($availableSponsorships as $sponsorship)
+				<div class="text-center w-50">
+					<div class="card-body">
+
+						<div class="card__bx card-color">
+							<div class="card__data">
+								<div class="card__icon">
+									<div class="card__icon-bx">
+										<i class="fa-solid fa-pen-ruler"></i>
+									</div>
+								</div>
+								<div class="card__content">
+									<h3 class="m-0">{{ $sponsorship->level }}</h3>
+									<h5 class="card-title">{{ $sponsorship->duration }}</h5>
+								</div>
+							</div>
+						</div>
 
 
-        <div class="d-flex gap-1 mt-3">
-            <a class="btn btn-secondary" href="{{ route('admin.apartments.index') }}">Back</a>
-        </div>
-    </div>
+						{{-- <h5 class="card-title">{{ $sponsorship->duration }}</h5>
+                        <h6 class="card-title mt-3 mb-3" style="color: #9153a9;">{{ $sponsorship->price }}</h6> --}}
+						<a href="#" class="btn myBtnPurple" style="background-color: #485ba1; color: white;">Buy</a>
+					</div>
+				</div>
+			@endforeach
+		</div>
+
+
+		<div class="d-flex gap-1 mt-3">
+			<a class="btn btn-secondary" href="{{ route('admin.apartments.index') }}">Back</a>
+		</div>
+	</div>
 @endsection
