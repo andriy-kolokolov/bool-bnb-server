c2a6dfca01e7edf700a80618bec9284296ec4b6b
alessandropecchini99 <pecchini99@gmail.com>
2023-09-06 15:20:27 +0200

    try

--- resources/views/admin/apartments/sponsorship/index.blade.php
+++ resources/views/admin/apartments/sponsorship/index.blade.php
@@ -2,27 +2,26 @@
 @section('content')
     <div class="body">
         <h1 class="text-accent">pricing</h1>
-        <p class="fs-500">plans that work for everyone</p>
 
         <div class="plans">
 
-        
-            <div class="plan plan--light">
-                <h2 class="plan-title">basic</h2>
+            @foreach($availableSponsorships as $sponsorship)
+                <div class="plan plan--light">
+                    <h2 class="plan-title">basic</h2>
 
-                <p class="plan-price">$2.99</p>
+                    <p class="plan-price">$2.99</p>
 
-                <p class="plan-description">
-                    Eleifend cursus volutpat risus convallis nam sed
-                    quam sollicitudin eget leo at erat cursus justo
-                </p>
+                    <p class="plan-description">
+                        Eleifend cursus volutpat risus convallis nam sed
+                        quam sollicitudin eget leo at erat cursus justo
+                    </p>
 
-                <a href="#" class="btn-sponsor">Join Now</a>
-            </div>
+                    <a href="#" class="btn-sponsor">Join Now</a>
+                </div>
+            @endforeach
 
         </div>
 
-        <p class="fs-500 mb">Need something different? No problem!</p>
         <a href="#" class="btn-sponsor btn-mb">Get in touch</a>
     </div>
 @endsection
