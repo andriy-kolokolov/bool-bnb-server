187af58f44f9ed55dc021e0f38ada09001a75dd6
andriy-kolokolov <koloandre22@gmail.com>
2023-09-08 12:14:26 +0200

    small improvements

--- database/seeders/MessagesTableSeeder.php
+++ database/seeders/MessagesTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Message;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 
 class MessagesTableSeeder extends Seeder
 {
@@ -13,185 +14,200 @@ class MessagesTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create();
+
+        for ($i = 1; $i <= 60; $i++) {
+            $guestName = $faker->name;
+            $guestEmail = $faker->unique()->safeEmail;
+            $randApartmentId = random_int(1, 30);
+            $message = new Message([
+                'apartment_id' => $randApartmentId,
+                'guest_email' => $guestEmail,
+                'guest_name' => $guestName,
+                'message' => $faker->realText(100),
+            ]);
+            $message->save();
+        }
+
         $messages = [
             [
                 'apartment_id' => 1,
-                'guest_email'  => 'john.doe@example.com',
-                'guest_name'   => 'John Doe',
+                'guest_email'   => 'john.doe@example.com',
+                'guest_name'         => 'John Doe',
                 'message'      => 'Is this apartment available for the dates from August 10th to August 20th?',
             ],
             [
                 'apartment_id' => 2,
-                'guest_email'  => 'jane.smith@example.com',
-                'guest_name'   => 'Jane Smith',
+                'guest_email'   => 'jane.smith@example.com',
+                'guest_name'         => 'Jane Smith',
                 'message'      => 'What\'s the policy on pets? Can I bring my small dog with me?',
             ],
             [
                 'apartment_id' => 3,
-                'guest_email'  => 'sam.johnson@example.com',
-                'guest_name'   => 'Sam Johnson',
+                'guest_email'   => 'sam.johnson@example.com',
+                'guest_name'         => 'Sam Johnson',
                 'message'      => 'Are there any good hiking trails near the apartment?',
             ],
             [
                 'apartment_id' => 4,
-                'guest_email'  => 'emily.wilson@example.com',
-                'guest_name'   => 'Emily Wilson',
+                'guest_email'   => 'emily.wilson@example.com',
+                'guest_name'         => 'Emily Wilson',
                 'message'      => 'Is there a minimum stay requirement for this apartment?',
             ],
             [
                 'apartment_id' => 5,
-                'guest_email'  => 'michael.davis@example.com',
-                'guest_name'   => 'Michael Davis',
+                'guest_email'   => 'michael.davis@example.com',
+                'guest_name'         => 'Michael Davis',
                 'message'      => 'Can you provide more details about public transportation options in the area?',
             ],
             [
                 'apartment_id' => 6,
-                'guest_email'  => 'olivia.jones@example.com',
-                'guest_name'   => 'Olivia Jones',
+                'guest_email'   => 'olivia.jones@example.com',
+                'guest_name'         => 'Olivia Jones',
                 'message'      => 'What\'s the check-in and check-out time for this apartment?',
             ],
             [
                 'apartment_id' => 7,
-                'guest_email'  => 'william.brown@example.com',
-                'guest_name'   => 'William Brown',
+                'guest_email'   => 'william.brown@example.com',
+                'guest_name'         => 'William Brown',
                 'message'      => 'Is there a parking space available for guests?',
             ],
             [
                 'apartment_id' => 8,
-                'guest_email'  => 'sophia.miller@example.com',
-                'guest_name'   => 'Sophia Miller',
+                'guest_email'   => 'sophia.miller@example.com',
+                'guest_name'         => 'Sophia Miller',
                 'message'      => 'Do you offer any discounts for longer stays?',
             ],
             [
                 'apartment_id' => 9,
-                'guest_email'  => 'james.anderson@example.com',
-                'guest_name'   => 'James Anderson',
+                'guest_email'   => 'james.anderson@example.com',
+                'guest_name'         => 'James Anderson',
                 'message'      => 'Are there any good restaurants within walking distance?',
             ],
             [
                 'apartment_id' => 10,
-                'guest_email'  => 'oliver.wilson@example.com',
-                'guest_name'   => 'Oliver Wilson',
+                'guest_email'   => 'oliver.wilson@example.com',
+                'guest_name'         => 'Oliver Wilson',
                 'message'      => 'What\'s the nearest metro station to the apartment?',
             ],
             [
                 'apartment_id' => 11,
-                'guest_email'  => 'liam.wilson@example.com',
-                'guest_name'   => 'Liam Wilson',
+                'guest_email'   => 'liam.wilson@example.com',
+                'guest_name'         => 'Liam Wilson',
                 'message'      => 'Is there a security deposit required for the apartment?',
             ],
             [
                 'apartment_id' => 12,
-                'guest_email'  => 'ava.anderson@example.com',
-                'guest_name'   => 'Ava Anderson',
+                'guest_email'   => 'ava.anderson@example.com',
+                'guest_name'         => 'Ava Anderson',
                 'message'      => 'Is smoking allowed in the apartment?',
             ],
             [
                 'apartment_id' => 13,
-                'guest_email'  => 'noah.jackson@example.com',
-                'guest_name'   => 'Noah Jackson',
+                'guest_email'   => 'noah.jackson@example.com',
+                'guest_name'         => 'Noah Jackson',
                 'message'      => 'Are there any local events or festivals happening during my stay?',
             ],
             [
                 'apartment_id' => 14,
-                'guest_email'  => 'mia.smith@example.com',
-                'guest_name'   => 'Mia Smith',
+                'guest_email'   => 'mia.smith@example.com',
+                'guest_name'         => 'Mia Smith',
                 'message'      => 'Is there a gym or fitness center nearby?',
             ],
             [
                 'apartment_id' => 15,
-                'guest_email'  => 'logan.johnson@example.com',
-                'guest_name'   => 'Logan Johnson',
+                'guest_email'   => 'logan.johnson@example.com',
+                'guest_name'         => 'Logan Johnson',
                 'message'      => 'Can you recommend any nearby tourist attractions?',
             ],
             [
                 'apartment_id' => 16,
-                'guest_email'  => 'chloe.davis@example.com',
-                'guest_name'   => 'Chloe Davis',
+                'guest_email'   => 'chloe.davis@example.com',
+                'guest_name'         => 'Chloe Davis',
                 'message'      => 'Is there a cleaning fee for the apartment?',
             ],
             [
                 'apartment_id' => 17,
-                'guest_email'  => 'ethan.brown@example.com',
-                'guest_name'   => 'Ethan Brown',
+                'guest_email'   => 'ethan.brown@example.com',
+                'guest_name'         => 'Ethan Brown',
                 'message'      => 'Do you provide airport transportation services?',
             ],
             [
                 'apartment_id' => 18,
-                'guest_email'  => 'luna.wilson@example.com',
-                'guest_name'   => 'Luna Wilson',
+                'guest_email'   => 'luna.wilson@example.com',
+                'guest_name'         => 'Luna Wilson',
                 'message'      => 'What\'s the cancellation policy for this apartment?',
             ],
             [
                 'apartment_id' => 19,
-                'guest_email'  => 'oliver.miller@example.com',
-                'guest_name'   => 'Oliver Miller',
+                'guest_email'   => 'oliver.miller@example.com',
+                'guest_name'         => 'Oliver Miller',
                 'message'      => 'Is there a balcony with a view in the apartment?',
             ],
             [
                 'apartment_id' => 20,
-                'guest_email'  => 'lily.jones@example.com',
-                'guest_name'   => 'Lily Jones',
+                'guest_email'   => 'lily.jones@example.com',
+                'guest_name'         => 'Lily Jones',
                 'message'      => 'Are there any nearby shopping malls or markets?',
             ],
             [
                 'apartment_id' => 21,
-                'guest_email'  => 'benjamin.davis@example.com',
-                'guest_name'   => 'Benjamin Davis',
+                'guest_email'   => 'benjamin.davis@example.com',
+                'guest_name'         => 'Benjamin Davis',
                 'message'      => 'Is there a grocery store within walking distance?',
             ],
             [
                 'apartment_id' => 22,
-                'guest_email'  => 'emily.anderson@example.com',
-                'guest_name'   => 'Emily Anderson',
+                'guest_email'   => 'emily.anderson@example.com',
+                'guest_name'         => 'Emily Anderson',
                 'message'      => 'What\'s the policy on late check-in or early check-out?',
             ],
             [
                 'apartment_id' => 23,
-                'guest_email'  => 'henry.martin@example.com',
-                'guest_name'   => 'Henry Martin',
+                'guest_email'   => 'henry.martin@example.com',
+                'guest_name'         => 'Henry Martin',
                 'message'      => 'Are there any nearby parks or recreational areas?',
             ],
             [
                 'apartment_id' => 24,
-                'guest_email'  => 'sophia.taylor@example.com',
-                'guest_name'   => 'Sophia Taylor',
+                'guest_email'   => 'sophia.taylor@example.com',
+                'guest_name'         => 'Sophia Taylor',
                 'message'      => 'Is there a shuttle service to the airport?',
             ],
             [
                 'apartment_id' => 25,
-                'guest_email'  => 'william.wilson@example.com',
-                'guest_name'   => 'William Wilson',
+                'guest_email'   => 'william.wilson@example.com',
+                'guest_name'         => 'William Wilson',
                 'message'      => 'Do you offer any special discounts for families with children?',
             ],
             [
                 'apartment_id' => 26,
-                'guest_email'  => 'zoey.miller@example.com',
-                'guest_name'   => 'Zoey Miller',
+                'guest_email'   => 'zoey.miller@example.com',
+                'guest_name'         => 'Zoey Miller',
                 'message'      => 'Is there a dishwasher in the apartment?',
             ],
             [
                 'apartment_id' => 27,
-                'guest_email'  => 'daniel.anderson@example.com',
-                'guest_name'   => 'Daniel Anderson',
+                'guest_email'   => 'daniel.anderson@example.com',
+                'guest_name'         => 'Daniel Anderson',
                 'message'      => 'Is there a coffee maker in the kitchen?',
             ],
             [
                 'apartment_id' => 28,
-                'guest_email'  => 'olivia.martin@example.com',
-                'guest_name'   => 'Olivia Martin',
+                'guest_email'   => 'olivia.martin@example.com',
+                'guest_name'         => 'Olivia Martin',
                 'message'      => 'Is there a swimming pool in the apartment complex?',
             ],
             [
                 'apartment_id' => 29,
-                'guest_email'  => 'ethan.taylor@example.com',
-                'guest_name'   => 'Ethan Taylor',
+                'guest_email'   => 'ethan.taylor@example.com',
+                'guest_name'         => 'Ethan Taylor',
                 'message'      => 'What\'s the internet speed and connectivity like in the apartment?',
             ],
             [
                 'apartment_id' => 30,
-                'guest_email'  => 'ava.johnson@example.com',
-                'guest_name'   => 'Ava Johnson',
+                'guest_email'   => 'ava.johnson@example.com',
+                'guest_name'         => 'Ava Johnson',
                 'message'      => 'Is there a nearby pharmacy or medical facility?',
             ],
         ];
--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -3,155 +3,119 @@
 @endphp
 
 <header>
-	{{-- <div class="header-brand">
-		<a href="{{ route('admin.dashboard') }}">
-			<img class="img-logo" src="{{ asset('logo-orizzontale.png') }}" alt="Logo"
-				style="max-width: 150px; padding: 1rem;">
-		</a>
-	</div>
-	<nav class="navbar navbar-expand-lg bg-body-tertiary flex-end">
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-			aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+    <nav>
+        <div class="myContainer">
+            <!-- logo -->
+            <a class="image" href="{{route( 'admin.apartments.index')}}">
+                <img
+                    src="{{ asset('logo-orizzontale.png') }}"
+                    alt="logo"
+                    class="ms-total"
+                />
+                <img src="{{ asset('logo-b.png') }}" alt="logo-small" class="ms-small"/>
+            </a>
 
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav ms-auto">
-
-				<!--    ADMIN PROFILE EDIT    -->
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-						{{ $user->name }}
-					</a>
-					<ul class="dropdown-menu dropdown-menu-end  text-center">
-						<li>
-							<a class="dropdown-item w-100" href="{{ route('profile.edit') }}">Edit profile</a>
-						</li>
-						<li>
-							<form class="dropdown-item" action="{{ route('logout') }}" method="post">
-								@csrf
-								<button class="btn btn-danger w-100">Log out</button>
-							</form>
-						</li>
-					</ul>
-				</li>
-
-			</ul>
-		</div>
-	</nav> --}}
-
-	<nav>
-		<div class="myContainer">
-		<!-- logo -->
-		<a class="image" href="http://localhost:5174">
-			<img
-				src="{{ asset('logo-orizzontale.png') }}"
-				alt="logo"
-				class="ms-total"
-			/>
-			<img src="{{ asset('logo-b.png') }}" alt="logo-small" class="ms-small" />
-		</a>
-
-
-
-		<!-- routes -->
-		<div class="routes btn-group">
-			<div class="menu-dropdown">
-			<button
-				id="button"
-				type="button"
-				data-bs-toggle="dropdown"
-				data-bs-auto-close="inside"
-			>
-				<div id="icon"></div>
-			</button>
-			<ul class="dropdown-menu dropdown-menu-end">
-				<li>
-					<a class="dropdown-item" href="http://localhost:5174">
-						Home
-					</a>
-				</li>
-				<li>
-					<a class="dropdown-item w-100" href="{{ route('admin.dashboard') }}">Dashboard</a>
-				</li>
-				<li>
-					<a class="dropdown-item w-100" href="{{ route('profile.edit') }}">Edit profile</a>
-				</li>
-				<li>
-					<form class="dropdown-item" action="{{ route('logout') }}" method="post">
-						@csrf
-						<button class="btn btn-danger w-100">Log out</button>
-					</form>
-				</li>
-			</ul>
-			</div>
-			<!-- altra routes -->
-			<div class="menu-off">
-			<button
-				id="btn"
-				type="button"
-				data-bs-toggle="offcanvas"
-				data-bs-target="#staticBackdrop"
-				aria-controls="offcanvasRight"
-			>
-				<div id="icn"></div>
-			</button>
-			<div
-				class="offcanvas offcanvas-end w-100"
-				data-bs-backdrop="static"
-				tabindex="-1"
-				id="staticBackdrop"
-				aria-labelledby="staticBackdropLabel"
-			>
-				<div class="offcanvas-header btn-position">
-				<button
-					type="button"
-					class="btn"
-					id="off-close"
-					data-bs-dismiss="offcanvas"
-					aria-label="Close"
-				>
-					<i class="fa-solid fa-xmark"></i>
-				</button>
-				</div>
-				<div class="offcanvas-body">
-				<div class="ms-routes">
-					<ul>
-						<li>
-							<button data-bs-dismiss="offcanvas" aria-label="Close" href="http://localhost:5174">
-								<div  class="drop-item">
-									Home
-								</div>
-							</button>
-						</li>
-						<li>
-							<button data-bs-dismiss="offcanvas" aria-label="Close" href="{{ route('admin.dashboard') }}">
-								<div  class="drop-item">
-									Dashboard
-								</div>
-							</button>
-						</li>
-						<li>
-							<button data-bs-dismiss="offcanvas" aria-label="Close" href="{{ route('profile.edit') }}">
-								<div  class="drop-item">
-									Edit profile
-								</div>
-							</button>
-						</li>
-						<li>
-							<button data-bs-dismiss="offcanvas" aria-label="Close" class="drop-item">
-								<form action="{{ route('logout') }}" method="post">
-									@csrf
-									<button>Log out</button>
-								</form>
-							</button>
-						</li>
-					</ul>
-				</div>
-				</div>
-			</div>
-			</div>
-		</div>
-		</div>
-	</nav>
+            <!-- routes -->
+            <div class="routes btn-group">
+                <div class="menu-dropdown">
+                    <button
+                        id="button"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        data-bs-auto-close="inside"
+                    >
+                        <div id="icon"></div>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li>
+                            <a class="dropdown-item" href="http://localhost:5174">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item w-100" href="{{ route('admin.dashboard') }}">Dashboard</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item w-100" href="{{ route('profile.edit') }}">Edit profile</a>
+                        </li>
+                        <li>
+                            <form class="dropdown-item" action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button class="btn btn-danger w-100">Log out</button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+                <!-- altra routes -->
+                <div class="menu-off">
+                    <button
+                        id="btn"
+                        type="button"
+                        data-bs-toggle="offcanvas"
+                        data-bs-target="#staticBackdrop"
+                        aria-controls="offcanvasRight"
+                    >
+                        <div id="icn"></div>
+                    </button>
+                    <div
+                        class="offcanvas offcanvas-end w-100"
+                        data-bs-backdrop="static"
+                        tabindex="-1"
+                        id="staticBackdrop"
+                        aria-labelledby="staticBackdropLabel"
+                    >
+                        <div class="offcanvas-header btn-position">
+                            <button
+                                type="button"
+                                class="btn"
+                                id="off-close"
+                                data-bs-dismiss="offcanvas"
+                                aria-label="Close"
+                            >
+                                <i class="fa-solid fa-xmark"></i>
+                            </button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <div class="ms-routes">
+                                <ul>
+                                    <li>
+                                        <button data-bs-dismiss="offcanvas" aria-label="Close"
+                                                href="http://localhost:5174">
+                                            <div class="drop-item">
+                                                Home
+                                            </div>
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button data-bs-dismiss="offcanvas" aria-label="Close"
+                                                href="{{ route('admin.dashboard') }}">
+                                            <div class="drop-item">
+                                                Dashboard
+                                            </div>
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button data-bs-dismiss="offcanvas" aria-label="Close"
+                                                href="{{ route('profile.edit') }}">
+                                            <div class="drop-item">
+                                                Edit profile
+                                            </div>
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button data-bs-dismiss="offcanvas" aria-label="Close" class="drop-item">
+                                            <form action="{{ route('logout') }}" method="post">
+                                                @csrf
+                                                <button>Log out</button>
+                                            </form>
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 </header>
