d10d66638ed44e2661dc5a5543ae1b5a8650fcfd
alessandropecchini99 <pecchini99@gmail.com>
2023-09-08 17:16:07 +0200

    finish dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,19 +3,19 @@
 @extends('admin.layouts.base')
 
 @section('content')
-    <div class="w-75 m-auto text-center d-flex flex-column align-items-center">
-        <h1 class="page-title fw-bold mt-4">
+    <div class="h-100 m-auto text-center d-flex flex-column align-items-center justify-content-center ms-dashboard">
+        <h1 class="page-title fw-bold">
             Welcome {{ $user->name }}!
         </h1>
-        <h4 class="mt-5">Use the buttons below to add a new apartment</h4>
+        <h4 class="mt-4">Use the buttons below to add a new apartment</h4>
         <h4>or to view a list of the ones you already added:</h4>
         <h4 class="mb-5">from there you will be able to perform various operations on each of them.</h4>
-        <div class="w-50 d-flex justify-content-evenly">
+        <div class="d-flex justify-content-evenly ms-dash-btn">
             <a href="{{ route('admin.apartments.create') }}" class="btn px-4 py-3 fw-bold"
-               style="background-color: #485ba1; color: white"><i class="bi bi-file-earmark-plus"></i> Add New Apartment
+               style="background-color: #485ba1; color: white"><i class="bi bi-file-earmark-plus"></i> Add Apartment
             </a>
             <a href="{{ route('admin.apartments.index') }}" class="btn px-4 py-3 fw-bold"
-               style="background-color: #9153a9; color: white"><i class="bi bi-file-earmark-plus"></i> View Your
+               style="background-color: #9153a9; color: white"><i class="bi bi-file-earmark-plus"></i> Your
                 Apartments</a>
         </div>
     </div>
