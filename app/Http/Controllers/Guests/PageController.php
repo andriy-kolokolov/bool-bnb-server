7d25ca7ef0e3640f814e91edc366ab606e7e99ce
andriy-kolokolov <koloandre22@gmail.com>
2023-09-09 21:30:16 +0200

    add auth check before redirect to dashboard view

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class PageController extends Controller
-{
-    public function home()
-    {
-        return view('admin.dashboard');
+class PageController extends Controller {
+    public function home() {
+        if (!Auth::user()) {
+            return view('auth.login');
+        } else {
+            return view('admin.dashboard');
+        }
     }
 }
